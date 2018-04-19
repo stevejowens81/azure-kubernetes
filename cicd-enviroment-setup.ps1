6033af845d8d331b22b6cc232ffeac884258c9a1
Steve Owens <steve.owens@bjss.com>
2018-04-19 12:27:23 +0100

    Update

--- cicd-enviroment-setup.ps1
+++ cicd-enviroment-setup.ps1
@@ -45,23 +45,23 @@ Param(
     [string] $KUBRGName = $KUBClusterName + '-RG'
 )
 
-Write-host $env:VarOMSRGName
-Write-host $env:VarOMSName
-Write-host $env:VarOMSLocation
-Write-host $env:VarKUBRGName
-Write-host $env:VarKUBClusterName
-Write-host $env:VarKUBLocation
+Write-host $env:OMSRGName
+Write-host $env:OMSName
+Write-host $env:OMSLocation
+Write-host $env:KUBRGName
+Write-host $env:KUBClusterName
+Write-host $env:KUBLocation
 
-Write-Output  ("##vso[task.setvariable variable=VarOMSRGName]$OMSRGName")
-Write-Output  ("##vso[task.setvariable variable=VarOMSName]$OMSName")
-Write-Output  ("##vso[task.setvariable variable=VarOMSLocation]$OMSLocation")
-Write-Output  ("##vso[task.setvariable variable=VarKUBRGName]$KUBRGName")
-Write-Output  ("##vso[task.setvariable variable=VarKUBClusterName]$KUBClusterName")
-Write-Output  ("##vso[task.setvariable variable=VarKUBLocation]$KUBLocation")
+Write-Output  ("##vso[task.setvariable variable=OMSRGName]$OMSRGName")
+Write-Output  ("##vso[task.setvariable variable=OMSName]$OMSName")
+Write-Output  ("##vso[task.setvariable variable=OMSLocation]$OMSLocation")
+Write-Output  ("##vso[task.setvariable variable=KUBRGName]$KUBRGName")
+Write-Output  ("##vso[task.setvariable variable=KUBClusterName]$KUBClusterName")
+Write-Output  ("##vso[task.setvariable variable=KUBLocation]$KUBLocation")
 
-Write-host $env:VarOMSRGName
-Write-host $env:VarOMSName
-Write-host $env:VarOMSLocation
-Write-host $env:VarKUBRGName
-Write-host $env:VarKUBClusterName
-Write-host $env:VarKUBLocation
+Write-host $env:OMSRGName
+Write-host $env:OMSName
+Write-host $env:OMSLocation
+Write-host $env:KUBRGName
+Write-host $env:KUBClusterName
+Write-host $env:KUBLocation
