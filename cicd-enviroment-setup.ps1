96f039f768cc21d7ffe797773cfb7e2050a638ed
Steve Owens <steve.owens@bjss.com>
2018-04-19 11:30:33 +0100

    Update

---
+++ cicd-enviroment-setup.ps1
@@ -0,0 +1,54 @@
+# Requires -Version 3.0
+# Content Version : 1.0.0.1
+# Creator - Steve Owens
+<#
+ .SYNOPSIS
+    Sets the VSTS pipeline parameters.
+
+ .DESCRIPTION
+    Sets the VSTS pipeline parameters.
+
+ .PARAMETER OMSName
+    x.
+
+ .PARAMETER KUBClusterName
+    x.
+
+ .PARAMETER x
+    x.
+
+ .PARAMETER x
+    x.
+
+ .PARAMETER x
+    x.
+
+ .PARAMETER x
+    x.
+
+ .PARAMETER x
+    x.
+
+ .PARAMETER x
+    x.
+
+ .PARAMETER x
+    x.
+
+#>
+Param(
+    [string] $OMSName = 'BJSS-DEMO-OMS',
+    [string] $OMSLocation = 'West Europe',
+    [string] $KUBClusterName = 'BJSS-DEMO-ACS',
+	[string] $KUBLocation = 'West Europe',
+    [string] $OMSRGName = $OMSName + '-RG',
+    [string] $KUBRGName = $KUBClusterName + '-RG',
+
+)
+
+Write-Host "##vso[task.setvariable variable=KUBClusterName]$OMSRGName"
+Write-Host "##vso[task.setvariable variable=OMSName]$OMSName"
+Write-Host "##vso[task.setvariable variable=OMSLocation]$OMSLocation"
+Write-Host "##vso[task.setvariable variable=KUBRGName]$KUBRGName"
+Write-Host "##vso[task.setvariable variable=KUBClusterName]$KUBClusterName"
+Write-Host "##vso[task.setvariable variable=KUBLocation]$KUBLocation"
